c2b120b4f926910c3dcf2230d1cb50a0744a2a4a
WILLIYO132\Guillermo <guillermo.gomez.olivares@gmail.com>
2017-02-13 20:50:19 +0100

    Añadidas funciones para explotar diamantes(Esta mal e incompleta) y tambien comprobar tablero para explotar si son iguales

--- cuda/jewelsLegendC/jewelsLegendC/jewelsLegendC.cpp
+++ cuda/jewelsLegendC/jewelsLegendC/jewelsLegendC.cpp
@@ -12,6 +12,36 @@ const int nColores = 8;
 
 void printDiamante(Diamante *diam, HANDLE hConsole);
 
+Diamante *explotarIguales(Diamante *diam, int posX, int posY, int filaExp, int columnaExp) {
+
+	if (diam[(posX + 1)*columnaExp + posY].color == diam[posX*columnaExp + posY].color)
+		diam[(posX + 1)*columnaExp + posY].color = 0;
+
+	if (diam[(posX - 1)*columnaExp + posY].color == diam[posX*columnaExp + posY].color)
+		diam[(posX - 1)*columnaExp + posY].color = 0;
+
+	if (diam[posX*columnaExp + posY + 1].color == diam[posX*columnaExp + posY].color)
+		diam[posX*columnaExp + posY + 1].color = 0;
+
+	if (diam[posX*columnaExp + posY - 1].color == diam[posX*columnaExp + posY].color)
+		diam[posX*columnas + posY - 1].color = 0;
+
+
+	diam[posX*columnas + posY].color = 0;
+
+
+	return diam;
+}
+Diamante *comprobarIguales(Diamante *diam) {
+	for (int i = 0; i < filas; i++) {  //Recorremos el array en busca de cadenas de tres numeros iguales
+		for (int j = 0; j < columnas; j++) {
+
+		}
+	}
+
+	return diam;
+}
+
 void inicicializarArray(Diamante *diam){
 	int numAleatorio = 0;
 
@@ -36,7 +66,31 @@ int main()
 	hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
 
 	inicicializarArray(diam);
+	diam = comprobarIguales(diam);
 	printDiamante(diam, hConsole);
+
+	while (true) {
+
+		cout << "\n\n\n";
+		SetConsoleTextAttribute(hConsole, 15);
+		cout << "Introduce la posicion del bloque que quieras explotar o 99 para salir";
+		int filaInt = 0, columnaInt = 0;
+		cout << "\nfila: ";
+		cin >> filaInt;
+
+		if (filaInt == 99)
+			exit(0);
+
+		cout << "columna: ";
+		cin >> columnaInt;
+
+
+			diam = explotarIguales(diam, filaInt, columnaInt, filas, columnas);
+			int size = filas*columnas * sizeof(diam);
+
+		//*moveBlocks(bloques, filas, columnas);
+		printDiamante(diam, hConsole);
+	}
 	getchar();
     return 0;
 }
